496f5235abe9337b9308739a07f93fee20e5187e
justine <caraojustine138@gmail.com>
2025-11-05 23:51:07 +0800

    basic setup, shadcn ui, theme-provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import ThemToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="fon-bold text-blue-500">Welcome In GoShop</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end"></div>
+      <ThemToggle/>
+      <h1 className="fon-bold text-blue-500 font-barlow">
+        Welcome In GoShop
+        </h1>
+        <Button variant="destructive">Click here</Button>
     </div>
   );
 }
